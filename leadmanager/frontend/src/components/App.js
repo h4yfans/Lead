3012ee9ea8f1824f9f9af71d3249819dbac284f2
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-01 19:13:04 +0300

    load user action added get user from request if its authenticated

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -13,7 +13,7 @@ import PrivateRoute from './common/PrivateRoute'
 
 import {Provider} from 'react-redux'
 import store from '../store'
-
+import {loadUser} from "../actions/auth";
 
 //Alert Options
 const alertOptions = {
@@ -22,6 +22,10 @@ const alertOptions = {
 };
 
 class App extends Component {
+    componentDidMount(){
+        store.dispatch(loadUser())
+    }
+
     render() {
         return (
             <Provider store={store}>
